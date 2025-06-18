51cfa0c6d0dbd0f4210c0d653d740664435343ca
Pebi Pebriansah <pebipebriansah160200@gmail.com>
2025-06-18 09:13:41 +0700

    Update cabaiImages.js

--- src/assets/cabaiImages.js
+++ src/assets/cabaiImages.js
@@ -1,9 +1,9 @@
 // assets/cabaiImages.js
 import cabaiHijau from './cabai_hijau.jpg'; // Ganti dengan path gambar yang sesuai
-import cabeMerah from './cabe_merah.jpg';
-import cabeMerahKeriting from './cabe_merah_keriting.jpg';
-import cabeRawitHijau from './cabe_rawit_hijau.jpg';
-import cabeRawitMerah from './cabe_rawit_merah.jpg';
+import cabeMerah from './cabai_merah.jpg';
+import cabeMerahKeriting from './cabai_merah_keriting.jpg';
+import cabeRawitHijau from './cabai_rawit_hijau.jpg';
+import cabeRawitMerah from './cabai_rawit_merah.jpg';
 
 const cabaiImages = {
   'Cabai Hijau Biasa': cabaiHijau,
